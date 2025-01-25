6ca7d4f35fe48fa7ea378dd4c9e5344fc6c66cd3
Augustine Joseph <169073772+augustine-aj@users.noreply.github.com>
2025-01-25 21:35:17 +0530

    Add files via upload

---
+++ Day 6/sample_server.js
@@ -0,0 +1,15 @@
+const http = require('http')
+const fs = require('fs')
+
+http.createServer(
+    (req, res) =>{
+        fs.readFile(
+            'index.html',
+            (err, data) =>{
+                res.writeHead(200, {'Content-Type': 'text/html'})
+                res.write(data)
+                res.end()
+            }
+        )
+    }
+).listen(7000, () => console.log('Server started...'))
